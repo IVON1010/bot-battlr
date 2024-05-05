a4bcac8638009bcf916029fb4d4434806ad7570b
Yvonne <ndunguyvonne10@gmail.com>
2024-05-05 22:35:37 +0300

    add clickhandler

--- src/components/BotCard.jsx
+++ src/components/BotCard.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+
+
 const botTypeClasses = {
   Assault: "icon military",
   Defender: "icon shield",
@@ -7,11 +10,54 @@ const botTypeClasses = {
   Captain: "icon star",
 };
 
-
-function BotCard() {
+function BotCard({ bot, clickHandler, handleDelete }) {
   return (
-   
-  )
+    <div className="ui column flex items-stretch ...">
+      <div className="ui card" key={bot.id} onClick={() => clickHandler(bot)}>
+        <div className="image ">
+          <img alt="oh no!" src={bot.avatar_url} />
+        </div>
+        <div className="content">
+          <div className="header">
+            {bot.name}
+            <i className={botTypeClasses[bot.bot_class]} />
+          </div>
+          <div className="meta text-wrap">
+            <small>{bot.catchphrase.substring(0, 32)}</small>
+          </div>
+        </div>
+        <div className="extra content">
+          <span>
+            <i className="icon heartbeat" />
+            {bot.health}
+          </span>
+
+          <span>
+            <i className="icon lightning" />
+            {bot.damage}
+          </span>
+          <span>
+            <i className="icon shield" />
+            {bot.armor}
+          </span>
+          <span>
+            <div className="ui center aligned segment basic">
+              <button
+                title="Delete this bot"
+                className="bg-red-600 w-5"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleDelete(bot);
+                }}
+              >
+                x
+              </button>
+            </div>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default BotCard
+export default BotCard;

9dfa6b5a6a5498c93dab085aea538841ed742e9c
Yvonne <ndunguyvonne10@gmail.com>
2024-05-05 22:22:18 +0300

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import BotsPage from "./components/BotsPage"
+
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BotsPage />
   )
 }
 

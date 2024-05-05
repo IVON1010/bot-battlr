908d9cb81bf60078fb445e12bd6f18ac10acd669
Yvonne <ndunguyvonne10@gmail.com>
2024-05-05 22:46:42 +0300

    add collection

--- src/components/YourBotArmy.jsx
+++ src/components/YourBotArmy.jsx
@@ -1,1 +1,12 @@
 import BotCard from "./BotCard";
+
+unction YourBotArmy({ collection}) {
+
+  return (
+    <div className="ui segment inverted olive bot-army bg-green-500">
+      <div className="ui five column grid">
+        {"Click on a Bot on the bot collection to enlist it to your army."}
+      </div>
+    </div>
+  );
+}

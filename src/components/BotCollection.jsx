50b7b02876e8cadc633ab357ac8a1d71ad2779d5
Yvonne <ndunguyvonne10@gmail.com>
2024-05-05 22:38:46 +0300

    add bot collection

--- src/components/BotCollection.jsx
+++ src/components/BotCollection.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import BotCard from "./BotCard";
 
-function BotCollection() {
+function BotCollection({ collection, clickHandler, handleDelete }) {
+  // Your code here
   return (
-    <div>BotCollection</div>
-  )
+    <div className="ui four column grid">
+      {"Bot Collection click on a bot to add it to your army."}
+      <div className="row">
+        {collection.map((bot) => (
+          <BotCard key={bot.id} bot={bot} clickHandler={clickHandler} handleDelete={handleDelete} className="grid grid-cols-4 gap-4"/>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default BotCollection
+export default BotCollection;

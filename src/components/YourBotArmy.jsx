f448cbac06a60d5fdcf8157c80112c8eddc76c33
Yvonne <ndunguyvonne10@gmail.com>
2024-05-05 22:47:25 +0300

    add clickhandler and handdelete

--- src/components/YourBotArmy.jsx
+++ src/components/YourBotArmy.jsx
@@ -1,11 +1,16 @@
 import BotCard from "./BotCard";
 
-unction YourBotArmy({ collection}) {
+unction YourBotArmy({ collection, clickHandler, handeleDelete }) {
 
   return (
     <div className="ui segment inverted olive bot-army bg-green-500">
       <div className="ui five column grid">
         {"Click on a Bot on the bot collection to enlist it to your army."}
+        <div className="row bot-army-row">
+          {collection.map((bot) => (
+            <BotCard key={bot.id} bot={bot} clickHandler={clickHandler} handleDelete={handeleDelete} />
+          ))}
+        </div>
       </div>
     </div>
   );

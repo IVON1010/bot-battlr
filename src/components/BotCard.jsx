4b1ac7dc8e4ce0dc11482b7ee6cd32be5f8f6f3f
Yvonne <ndunguyvonne10@gmail.com>
2024-05-05 22:26:49 +0300

    bot classes

--- src/components/BotCard.jsx
+++ src/components/BotCard.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+const botTypeClasses = {
+  Assault: "icon military",
+  Defender: "icon shield",
+  Support: "icon plus circle",
+  Medic: "icon ambulance",
+  Witch: "icon magic",
+  Captain: "icon star",
+};
+
 
 function BotCard() {
   return (
-    <div>BotCard</div>
+   
   )
 }
 
